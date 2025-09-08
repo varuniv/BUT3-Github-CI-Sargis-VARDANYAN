9d4b1edbcc85a5be117f30570371abe6728a2f75
Vardanyan Sargis <143796672+varuniv@users.noreply.github.com>
2025-09-08 16:45:14 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,9 @@ from greeting.main import greet
 from greeting.utils import add
 
 def test_greet():
+    """test great"""
     assert greet("World") == "Hello, World!"
 
 def test_add():
+    """test add"""
     assert add(5,7) == 12
