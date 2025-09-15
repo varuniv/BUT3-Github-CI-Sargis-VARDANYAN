896c1b14857d939dc6222c6abd6f1538db4334f8
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-09-15 12:07:37 +0200

    maj test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,14 +3,15 @@
 
 from greeting.main import greet
 from greeting.utils import add
+
+
 def test_greet():
     """test great"""
 
-
     assert greet("World") == "Hello, World!"
+
 
 def test_add():
     """test add"""
 
-
-    assert add(5,7) == 12
+    assert add(5, 7) == 12
